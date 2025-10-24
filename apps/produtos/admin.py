fc0630eec5bdbac1190a7772e3da6634ac6512fe
edmelo <ednaldo_melo@hotmail.com>
2025-10-24 11:42:49 -0300

    app_produtos

---
+++ apps/produtos/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Produto
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'categoria', 'preco', 'estoque', 'ativo']
+    list_filter = ['categoria', 'ativo']
+    search_fields = ['nome', 'descricao']
